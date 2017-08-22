86f0fb306ab792d611a53115e3caf696c5be0b9b
Florence <smc.florence@gmail.com>
2017-08-22 11:17:01 -0400

    added isbn to books table

--- db/migrations/20170822005202_create_books.js
+++ db/migrations/20170822005202_create_books.js
@@ -1,7 +1,8 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('books', function(table) {
-    table.increments('id'); // should be isbn?
+    table.increments('id');
+    table.integer('isbn');
     table.string('category').notNullable();
     table.string('title').notNullable();
     table.string('abstract');
